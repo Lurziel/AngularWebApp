aba26f693c40294cdaffa5b687c58c0e937618ee
Charles-Eric DEPAZ <ce.depaz@student.helmo.be>
2025-03-29 14:04:50 +0100

    change projects order

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -12,6 +12,21 @@ export class HomePageComponent {
 
   projects: any[] = [
     {
+      name: "GRICOR QCM",
+      description: "Site permettant aux professeurs d'Helmo de corriger automatiquement des QCM au format papier. J'ai travaillé dessus durant 2 années en compagnie de 4 autres analystes-développeurs. le frontend est réalisé en React et le backend en Python.",
+      img: "assets/images/qcm.jpg",
+    },
+    {
+      name: "Helmo tutorat",
+      description: "Site web et application mobile permettant aux élèves de s'inscrire à des tutorats ou d'en organiser. Le projet a été réalisé en binôme avec un autre analyste développeur. Le site est réalisé en React, l'application mobile en Flutter et le service REST en .Net.",
+      img: "assets/images/small-class.jpg",
+    },
+    {
+      name: "DungeonsHeros",
+      description: "Site web dans lequel on peut créer une équipe de héros pour les envoyer dans des donjons pour monter de niveau. Le concept est inspiré de donjons et dragons. Le site est réalisé uniquement en .Net.",
+      img: "assets/images/donjon-dragon.jpg",
+    },
+    {
       name: "Montage d'un raspberry",
       description: "Montage d'un raspberry pi 5, 8 gigas de RAM, avec un NMVe pour stocker l'OS et les données. L'objectif recherché est de faire des projets orientés IOT (internet des objets).",
       img: "assets/images/rasp-pi-5.jpg"
@@ -46,21 +61,6 @@ export class HomePageComponent {
       description: "Version moins développée du portfolio qui utilise les technologies React et JavaScript. Le design est réalisé avec le framework Bootstrap.",
       img: "assets/images/react.png"
       //href: "https://react-web-app-rosy.vercel.app/"
-    },
-    {
-      name: "GRICOR QCM",
-      description: "Site permettant aux professeurs d'Helmo de corriger automatiquement des QCM au format papier. J'ai travaillé dessus durant 2 années en compagnie de 4 autres analystes-développeurs. le frontend est réalisé en React et le backend en Python.",
-      img: "assets/images/qcm.jpg",
-    },
-    {
-      name: "Helmo tutorat",
-      description: "Site web et application mobile permettant aux élèves de s'inscrire à des tutorats ou d'en organiser. Le projet a été réalisé en binôme avec un autre analyste développeur. Le site est réalisé en React, l'application mobile en Flutter et le service REST en .Net.",
-      img: "assets/images/small-class.jpg",
-    },
-    {
-      name: "DungeonsHeros",
-      description: "Site web dans lequel on peut créer une équipe de héros pour les envoyer dans des donjons pour monter de niveau. Le concept est inspiré de donjons et dragons. Le site est réalisé uniquement en .Net.",
-      img: "assets/images/donjon-dragon.jpg",
     }
   ]
 }
